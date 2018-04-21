7fbff867ce293ee541cf2d45fff0b1ffc5b36e71
María Hernández <UO229803@uniovi.es>
2018-04-21 15:47:06 +0200

    UsuarioTable

--- app/src/main/java/com/isabeldelalamo_mariahernandez/andtv/data/db/Tables.kt
+++ app/src/main/java/com/isabeldelalamo_mariahernandez/andtv/data/db/Tables.kt
@@ -1,17 +1,9 @@
 package com.isabeldelalamo_mariahernandez.andtv.data.db
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ddd0a350efddf623a25481adcb541ed687ee065d
 object UsuarioTable {
     const val NAME = "Usuario"
     const val ID = "_id"
     const val EMAIL = "email"
     const val PASSWORD = "password"
     const val PELICULASFAVORITASID = "peliculasFavoritasID"
-<<<<<<< HEAD
-=======
-
->>>>>>> ddd0a350efddf623a25481adcb541ed687ee065d
 }
